0c567080b1fce81aec0176cad481824cbfa69951
crashing booth <jholtzkener@hotmail.com>
2018-09-17 07:04:47 -0400

    added ui elements

---
+++ MIDIToJSON/UploaderDownloaderVC.swift
@@ -0,0 +1,21 @@
+//
+//  UploaderDownloaderVC.swift
+//  MIDIToJSON
+//
+//  Created by Jeff Holtzkener on 2018-09-17.
+//  Copyright © 2018 crashingbooth. All rights reserved.
+//
+
+import UIKit
+
+class UploaderDownloaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
