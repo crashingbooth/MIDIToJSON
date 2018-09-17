45f6fe05018584bc29e1bd3b49ce3aa60b32bfaa
crashing booth <jholtzkener@hotmail.com>
2018-09-17 07:51:41 -0400

    firebase observers

--- MIDIToJSON/FirebaseManager.swift
+++ MIDIToJSON/FirebaseManager.swift
@@ -11,13 +11,38 @@ import Firebase
 
 class FirebaseManager: NSObject, UITableViewDataSource, UITableViewDelegate {
     var filenames = [String]()
+    var rootRef: DatabaseReference!
+    var fileIndexRef: DatabaseReference!
+    var filesRef: DatabaseReference!
+    weak var tableViewReloadDelegate: TableViewReloadDelegate?
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        
+        return filenames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        return UITableViewCell()
     }
     
+    fileprivate func setupReferences() {
+        rootRef = Database.database().reference(withPath: "root")
+        fileIndexRef = rootRef.child("fileindex")
+        filesRef = rootRef.child("files")
+    }
+    
+    fileprivate func setupObservers() {
+        fileIndexRef.observe(.value, with: { snapshot in
+            if let val = snapshot.value as? [String] {
+                self.filenames = val
+                self.tableViewReloadDelegate?.reloadData()
+            }
+            
+        })
+    }
+    
+}
+
+protocol TableViewReloadDelegate: class {
+    func reloadData()
 }
