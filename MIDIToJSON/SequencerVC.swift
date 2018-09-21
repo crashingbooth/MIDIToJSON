380a397aedf46498efda114396574371ef13b460
crashing booth <jholtzkener@hotmail.com>
2018-09-21 18:06:07 -0400

    started new vc

---
+++ MIDIToJSON/SequencerVC.swift
@@ -0,0 +1,17 @@
+//
+//  SequencerVC.swift
+//  MIDIToJSON
+//
+//  Created by Jeff Holtzkener on 2018-09-21.
+//  Copyright © 2018 crashingbooth. All rights reserved.
+//
+
+import UIKit
+
+class SequencerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
